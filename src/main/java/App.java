7299cd7fef17895f2b2d29ab00c7dad64e55c687
XIIIDark <alexartemyev13@gmail.com>
2023-01-07 13:08:39 +0600

    taki +cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
     }
 }
